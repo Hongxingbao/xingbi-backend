4f1ab27ea3898e1b67a2ad1c3fc0c3de012eb1c6
24840 <2484047829@qq.com>
2023-12-26 14:13:50 +0800

    AI生成失败后自动重试一次

--- src/main/java/com/sing/init/bimq/BiMessageConsumer.java
+++ src/main/java/com/sing/init/bimq/BiMessageConsumer.java
@@ -43,15 +43,16 @@ public class BiMessageConsumer {
     private RedissonClient redissonClient;
 
     /**
-     *
-     * @param message 接收到的消息内容。
-     * @param channel 与 RabbitMQ 通信的通道对象。
-     * @param deliveryTag 消息的交付标签，用来唯一标识消息的标签,用于手动确认消息。
+     * @param message     接收到的消息内容。
+     * @param channel     与 RabbitMQ 通信的通道对象。
+     * @param deliveryTag 消息的交付标签，用来唯一标识消息的标签,用于手动确认消息。初始值为1
      */
     // 指定程序监听的消息队列和确认机制
     @SneakyThrows //用于简化在方法中抛出异常的代码
-    @RabbitListener(queues = {BiMqConstant.BI_QUEUE_NAME}, ackMode = "MANUAL")//ackMode = "MANUAL"：指定消息确认模式为手动确认，即需要在方法中手动调用确认或拒绝消息的方法。
+    @RabbitListener(queues = {BiMqConstant.BI_QUEUE_NAME}, ackMode = "MANUAL")
+//ackMode = "MANUAL"：指定消息确认模式为手动确认，即需要在方法中手动调用确认或拒绝消息的方法。
     public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
+
         log.info("receiveMessage message = {}", message);
         if (StringUtils.isBlank(message)) {
             // 如果失败，消息拒绝
@@ -88,9 +89,11 @@ public class BiMessageConsumer {
 
         String[] splits = result.split("【【【【【");
         if (splits.length < 3) {
-            //此处也可以进行重试，但是有一定风险，万一AI一直生成不成功，则会反复调用AI接口，导致产生大量费用...
-            //channel.basicNack(deliveryTag, false, true);
-            channel.basicNack(deliveryTag, false, false);
+            //此处进行重试，最多重试一次
+            if (deliveryTag > 1) {
+                channel.basicNack(deliveryTag, false, false);
+            }
+            channel.basicNack(deliveryTag, false, true);
             handleChartUpdateError(chart.getId(), "AI 生成错误");
             return;
         }
